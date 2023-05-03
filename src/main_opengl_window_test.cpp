44f06dcf90fd62444f8a63f18256305f06875e11
Majorx234 <majorx234@googlemail.com>
2023-05-03 03:38:34 +0200

    Add format settings for window

--- src/main_opengl_window_test.cpp
+++ src/main_opengl_window_test.cpp
@@ -1,15 +1,22 @@
 #include <QApplication>
+#include <QSurfaceFormat>
 #include "opengl_window.hpp"
 
 int main(int argc, char* argv[])
 {
-    QApplication app(argc, argv);
+  QApplication app(argc, argv);
 
-    // For OpenGL Graphics
-    OpenGlWindow opengl_window;
-    opengl_window.resize(800,600);
-    opengl_window.show();
+	// Set OpenGL Version information
+	QSurfaceFormat format;
+	format.setRenderableType(QSurfaceFormat::OpenGL);
+	format.setProfile(QSurfaceFormat::CoreProfile);
+	format.setVersion(3,3);
+  // For OpenGL Graphics
+  OpenGlWindow opengl_window;
+  opengl_window.setFormat(format);
+  opengl_window.resize(800,600);
+  opengl_window.show();
 
-    return app.exec();
+  return app.exec();
 }
 
